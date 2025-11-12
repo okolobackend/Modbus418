c3b06db046bb24bebfaaf0b9aaa797270b4fd4c8
okolobackend <dmdmgol@ya.ru>
2025-11-12 21:30:55 +0300

    Подключаем мок-чайник и базовую команду

---
+++ MockDevices/Kettle.cs
@@ -0,0 +1,119 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Modbus418.MockDevices
+{
+    public class KettleMock : IDisposable
+    {
+        private readonly Random _random = new Random();
+        private readonly object _lock = new object();
+        private bool _isOn = false;
+        private bool _isDisposed = false;
+        private int _currentTemperature;
+        private int _maxHeatingTemperature = 100;
+        private CancellationTokenSource? _cts;
+        private const int MIN_TEMPERATURE = 20;
+        private const int MAX_TEMPERATURE = 100;
+        private const int HEATING_RATE = 1;
+        private const int HEATING_INTERVAL = 1000;
+        private const int COOLING_RATE = 1;
+        private const int COOLING_INTERVAL = 3000;
+
+        public int CurrentTemperature
+        {
+            get { lock (_lock) return _currentTemperature; }
+        }
+        public bool IsOn
+        {
+            get { lock (_lock) return _isOn; }
+        }
+
+        public void Start()
+        {
+            _currentTemperature = _random.Next(20, 90);
+            _cts = new CancellationTokenSource();
+
+            Task.Run(async () => await TemperatureUpdateLoop(_cts.Token));
+
+            Console.WriteLine("Чайник подключен к сети");
+            Console.WriteLine($"Температура воды в чайнике: {_currentTemperature}");
+        }
+
+        private async Task TemperatureUpdateLoop(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+
+                lock (_lock)
+                {
+                    // Нагрев чайника и автоматическое отключение по достижению максимальной температуры
+                    if (_isOn && _currentTemperature < _maxHeatingTemperature)
+                    {
+                        _currentTemperature = Math.Min(_currentTemperature + HEATING_RATE, _maxHeatingTemperature);
+                        if (_currentTemperature == _maxHeatingTemperature)
+                        {
+                            _isOn = false;
+                            Console.WriteLine("Чайник вскипел");
+                        }
+                        else if (_currentTemperature > _maxHeatingTemperature)
+                        {
+                            _isOn = false;
+                            Console.WriteLine($"Температура воды {_currentTemperature} превысила максимальное значение {_maxHeatingTemperature}");
+                        }
+                    }
+                    // Остывание до комнатной температуры
+                    else if (!_isOn && _currentTemperature > MIN_TEMPERATURE)
+                    {
+                        _currentTemperature = Math.Max(_currentTemperature - COOLING_RATE, MIN_TEMPERATURE);
+                        if (_currentTemperature % 5 == 0)
+                        {
+                            Console.WriteLine($"Вода остыла до {_currentTemperature}");
+                        }
+                    }
+                }
+                // Увеличиваем температуру раз в секунду, охлаждаемся раз в три
+                int updateInterval = _isOn ? HEATING_INTERVAL : COOLING_INTERVAL;
+                try
+                {
+                    await Task.Delay(updateInterval, token);
+                }
+                catch (TaskCanceledException)
+                {
+                    break;
+                }
+            }
+        }
+        // Пока что только вкл/выкл, но название(!) само за себя говорит!
+        public async Task ProcessModbusCommand()
+        {
+            await Task.Run(() =>
+            {
+                lock (_lock)
+                {
+                    if (!_isOn)
+                    {
+                        _isOn = true;
+                        Console.WriteLine("Чайник включен");
+                    }
+                    else
+                    {
+                        _isOn = false;
+                        Console.WriteLine("Чайник выключен");
+                    }
+                }
+            });
+            
+        }
+        public void Dispose()
+        {
+            if (!_isDisposed)
+            {
+                _cts?.Cancel();
+                _cts?.Dispose();
+                Console.WriteLine("Чайник не в сети");
+                _isDisposed = true;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Modbus418.MockDevices;
+
+namespace Modbus418
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            using var kettleMock = new KettleMock();
+            kettleMock.Start();
+
+            while (true)
+            {
+                Console.WriteLine("Выберите команду для чайника:");
+                Console.WriteLine("0 - Выйти из программы");
+                Console.WriteLine("1 - Включить чайник");
+                Console.Write("Номер команды: ");
+
+                if (!ushort.TryParse(Console.ReadLine(), out ushort commandId))
+                {
+                    Console.WriteLine("Ошибка ввода. Введите число");
+                    continue;
+                }
+
+                switch (commandId)
+                {
+                    case 0:
+                        Console.WriteLine("Завершение работы");
+                        return;
+
+                    case 1:
+                        await kettleMock.ProcessModbusCommand();
+                        break;
+                    default:
+                        Console.WriteLine("Неизвестная команда");
+                        break;
+                }
+            }
+        }
+    }
+}
